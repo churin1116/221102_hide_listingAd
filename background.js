eca20f11b2e8e7c97b3ab8f6979a2aa542e08c77
nakabayashi991116 <nakabayashi991116@gmail.com>
2023-02-17 10:50:18 +0900

    Bing 追加

--- background.js
+++ background.js
@@ -1,9 +1,20 @@
-// yahoo
-const elements = document.getElementsByClassName("sw-Card Ad js-Ad");
-for (let i = 0; i < elements.length; i++) {
-    // console.log(elements[i]);
-    elements[i].parentNode.style.display = 'none';
+const hideClassAds = (classname) => {
+    const elements = document.getElementsByClassName(classname);
+    for (let i = 0; i < elements.length; i++) {
+        // console.log(elements[i]);
+        elements[i].parentNode.style.display = 'none';
+    }
 }
+
+// yahoo
+hideClassAds('sw-Card Ad js-Ad');
 
 // google
 if (document.getElementById('taw')) document.getElementById('taw').style.display = 'none';
+
+// Bing
+const elements = document.getElementsByClassName('b_ad b_adTop');
+for (let i = 0; i < elements.length; i++) {
+    // console.log(elements[i]);
+    elements[i].style.display = 'none';
+}
